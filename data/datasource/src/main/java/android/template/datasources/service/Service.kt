52660950eaa1e97f01593d76dec205a0c77f3d6f
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-01 19:22:47 +0200

    FIX: Use "flowOn" to specify the Dispatchers.IO in Service.kt

--- data/datasource/src/main/java/android/template/datasources/service/Service.kt
+++ data/datasource/src/main/java/android/template/datasources/service/Service.kt
@@ -4,23 +4,20 @@ import android.template.datasources.apimodels.ProductApiModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.flowOn
 
 internal object Service {
 
     // TODO: Read data from the "data.json" file
     fun getData(): Flow<ProductApiModel> {
         return flow {
-            withContext(Dispatchers.IO) {
-                Thread.sleep(2000)
-                emit(
-                    ProductApiModel(
-                        id = 1,
-                        name = "Product 1",
-                        price = 234.56,
-                    ),
-                )
-            }
-        }
+            emit(
+                ProductApiModel(
+                    id = 1,
+                    name = "Product 1",
+                    price = 234.56,
+                ),
+            )
+        }.flowOn(Dispatchers.IO)
     }
 }
