b738c0e06923779acd276f9932d24a979e7335af
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-03 09:26:11 +0200

    ADD: Set ENABLE_COMPOSE_COMPILER_REPORTS to true only for debug builds of the Android Libraries

--- gradle-plugins/convention-plugins/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ gradle-plugins/convention-plugins/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -19,10 +19,15 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                 apply(libs.plugins.kotlin.compiler.convention.get().pluginId)
             }
 
-            extra.set(ENABLE_COMPOSE_COMPILER_REPORTS, true)
-
             extensions.configure<LibraryExtension> {
                 configureAndroid(this@with)
+                
+                buildTypes { 
+                    debug {
+                        extra.set(ENABLE_COMPOSE_COMPILER_REPORTS, true)
+                    }
+                }
+
                 defaultConfig.consumerProguardFile("consumer-rules.pro")
             }
         }
