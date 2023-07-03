f6c9eddeadf14a0cecb8dca05007ec232b49a60d
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-03 07:55:56 +0200

    ADD: Infinite loop of callas to the cat API

--- data/datasource/src/main/java/android/template/datasources/CatDataSourceImpl.kt
+++ data/datasource/src/main/java/android/template/datasources/CatDataSourceImpl.kt
@@ -3,8 +3,10 @@ package android.template.datasources
 import android.template.api.apimodels.CatApiModel
 import android.template.api.retrofit.NetworkClient
 import android.template.api.service.CatService
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 internal class CatDataSourceImpl(
     networkClient: NetworkClient,
@@ -14,6 +16,10 @@ internal class CatDataSourceImpl(
     override fun getCat(): Flow<CatApiModel> {
         return flow {
             emit(service.getCat())
-        }
+            while (true) {
+                Thread.sleep(3000)
+                emit(service.getCat())
+            }
+        }.flowOn(Dispatchers.IO)
     }
 }
