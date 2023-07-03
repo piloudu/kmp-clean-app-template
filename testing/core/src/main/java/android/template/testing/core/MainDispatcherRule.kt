d4d4ad2b9970df2063b4b0ea8063f3f67c783ea0
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-03 17:55:01 +0200

    ADD: MainDispatcherRule to the :testing:core module

---
+++ testing/core/src/main/java/android/template/testing/core/MainDispatcherRule.kt
@@ -0,0 +1,31 @@
+package android.template.testing.core
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * A JUnit [TestRule] that sets the Main dispatcher to [testDispatcher]
+ * for the duration of the test.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    lateinit var scheduler: TestCoroutineScheduler
+
+    override fun starting(description: Description) {
+        scheduler = testDispatcher.scheduler
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
