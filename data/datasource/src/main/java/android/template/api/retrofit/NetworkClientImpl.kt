f7acdc893ad431ef0ad3f41702c5a8f920374dc7
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-04 22:24:29 +0200

    ADD: Extract interface for the NetworkClient

---
+++ data/datasource/src/main/java/android/template/api/retrofit/NetworkClientImpl.kt
@@ -0,0 +1,40 @@
+package android.template.api.retrofit
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+class NetworkClientImpl(
+    baseUrl: String,
+    networkInterceptor: Interceptor? = null,
+    localInterceptor: Interceptor? = null,
+) : NetworkClient {
+    private val contentType: MediaType = "application/json".toMediaType()
+    private val json: Json = Json {
+        ignoreUnknownKeys = true
+    }
+    override val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(
+            buildOkHttpClient(
+                networkInterceptor = networkInterceptor,
+                localInterceptor = localInterceptor,
+            ),
+        )
+        .addConverterFactory(json.asConverterFactory(contentType))
+        .build()
+}
+
+private fun buildOkHttpClient(
+    networkInterceptor: Interceptor? = null,
+    localInterceptor: Interceptor? = null,
+): OkHttpClient {
+    return OkHttpClient.Builder().apply {
+        networkInterceptor?.let(::addNetworkInterceptor)
+        localInterceptor?.let(::addInterceptor)
+    }.build()
+}
--- data/datasource/src/main/java/android/template/di/DataSourcesModule.kt
+++ data/datasource/src/main/java/android/template/di/DataSourcesModule.kt
@@ -3,6 +3,7 @@ package android.template.di
 import android.template.api.apimodels.BASE_CAT_URL
 import android.template.api.interceptors.getCatLocalInterceptor
 import android.template.api.retrofit.NetworkClient
+import android.template.api.retrofit.NetworkClientImpl
 import android.template.datasources.CatDataSource
 import android.template.datasources.CatDataSourceImpl
 import android.template.datasources.ProductDataSource
@@ -15,8 +16,8 @@ import org.koin.dsl.module
 val dataSourcesModule: Module = module {
     factoryOf(::ProductDataSourceImpl) bind ProductDataSource::class
     factoryOf(::CatDataSourceImpl) bind CatDataSource::class
-    single {
-        NetworkClient(
+    single<NetworkClient> {
+        NetworkClientImpl(
             baseUrl = BASE_CAT_URL,
             localInterceptor = getCatLocalInterceptor(),
         )
