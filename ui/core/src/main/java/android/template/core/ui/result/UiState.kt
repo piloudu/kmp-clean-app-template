d0a1613b67727a0d679df23b50e757823b34dbf8
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-04 21:14:17 +0200

    ADD: Sealed interface UiState with type parameter

---
+++ ui/core/src/main/java/android/template/core/ui/result/UiState.kt
@@ -0,0 +1,21 @@
+package android.template.core.ui.result
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+sealed interface UiState<out T> {
+    data class Success<out T>(val data: T) : UiState<T>
+    data class Error(val exception: Throwable? = null) : UiState<Nothing>
+    object Loading : UiState<Nothing>
+}
+
+fun <T> Flow<T>.asUiState(): Flow<UiState<T>> {
+    return this
+        .map<T, UiState<T>> {
+            UiState.Success(it)
+        }
+        .onStart { emit(UiState.Loading) }
+        .catch { emit(UiState.Error(it)) }
+}
