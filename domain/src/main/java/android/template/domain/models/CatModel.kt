27bb2cb3c8d0a3028801662618898136c3665d6c
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-05 17:08:06 +0200

    ADD: Make domain models serializable

--- domain/src/main/java/android/template/domain/models/CatModel.kt
+++ domain/src/main/java/android/template/domain/models/CatModel.kt
@@ -1,16 +1,18 @@
 package android.template.domain.models
 
+import java.io.Serializable
+
 data class CatModel(
     val owner: Owner,
     val mimeType: MimeType,
     val url: Url,
-)
+) : Serializable
 
 @JvmInline
 value class Owner(val value: String)
 
 @JvmInline
-value class MimeType(val value: String)
+value class MimeType(val value: String) : Serializable
 
 @JvmInline
-value class Url(val value: String)
+value class Url(val value: String) : Serializable
--- domain/src/main/java/android/template/domain/models/ProductModel.kt
+++ domain/src/main/java/android/template/domain/models/ProductModel.kt
@@ -1,9 +1,11 @@
 package android.template.domain.models
 
+import java.io.Serializable
+
 data class ProductModel(
     val name: String,
     val price: Price
-)
+) : Serializable
 
 @JvmInline
-value class Price(val value: Double?)
+value class Price(val value: Double?) : Serializable
