427ff1dd07005350c6de6f0aaa7f53c43b080c43
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 11:29:52 +0200

    ADD: Use GetRickAndMortyUseCase in RickAndMortyViewModel.kt

--- ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyViewModel.kt
+++ ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyViewModel.kt
@@ -1,18 +1,25 @@
 package android.template.feature.main.ui.rickandmorty
 
 import android.template.core.ui.result.UiState
+import android.template.core.ui.result.asUiState
+import android.template.domain.models.RickAndMortyModel
+import android.template.domain.usecases.GetRickAndMortyUseCase
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class RickAndMortyViewModel(
+    getRickAndMortyUseCase: GetRickAndMortyUseCase,
 ) : ViewModel() {
-    val rickAndMortyUiState: StateFlow<UiState<RickAndMortyUiModel>> = MutableStateFlow(UiState.Loading)
-//        .map {  }
-//        .asUiState()
-//        .stateIn(
-//            scope = viewModelScope,
-//            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
-//            initialValue = UiState.Loading,
-//        )
+    val rickAndMortyUiState: StateFlow<UiState<RickAndMortyUiModel>> = getRickAndMortyUseCase()
+        .map(RickAndMortyModel::toUiModel)
+        .asUiState()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
+            initialValue = UiState.Loading,
+        )
 }
