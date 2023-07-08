8469b9e18057a138e8d457b1f5bd14e91ff21753
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 11:29:24 +0200

    ADD: Injection for the RickAndMortyViewModel

--- ui/feature-main/src/main/java/android/template/feature/main/di/FeatureMainModule.kt
+++ ui/feature-main/src/main/java/android/template/feature/main/di/FeatureMainModule.kt
@@ -2,6 +2,7 @@ package android.template.feature.main.di
 
 import android.template.feature.main.ui.cat.CatViewModel
 import android.template.feature.main.ui.products.MainProductsViewModel
+import android.template.feature.main.ui.rickandmorty.RickAndMortyViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -9,4 +10,5 @@ import org.koin.dsl.module
 val featureMainModule: Module = module {
     viewModelOf(::MainProductsViewModel)
     viewModelOf(::CatViewModel)
+    viewModelOf(::RickAndMortyViewModel)
 }
