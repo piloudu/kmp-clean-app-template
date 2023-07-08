5526ea97b8bd7e25c140447d7c495eb81b8c128e
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 14:42:32 +0200

    ADD: RickAndMortyNetworkClient and its DI logic

---
+++ data/datasource/src/main/java/android/template/api/retrofit/CatNetworkClient.kt
@@ -0,0 +1,21 @@
+package android.template.api.retrofit
+
+import okhttp3.Interceptor
+import retrofit2.Retrofit
+
+class CatNetworkClient(
+    baseUrl: String,
+    networkInterceptor: Interceptor? = null,
+    localInterceptor: Interceptor? = null,
+) : NetworkClient {
+    override val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(
+            buildOkHttpClient(
+                networkInterceptor = networkInterceptor,
+                localInterceptor = localInterceptor,
+            ),
+        )
+        .addConverterFactory(jsonConverterFactory)
+        .build()
+}
--- data/datasource/src/main/java/android/template/di/NetworkModule.kt
+++ data/datasource/src/main/java/android/template/di/NetworkModule.kt
@@ -1,16 +1,23 @@
 package android.template.di
 
 import android.template.api.apimodels.BASE_CAT_URL
+import android.template.api.apimodels.rickandmorty.RICK_AND_MORTY_BASE_URL
 import android.template.api.interceptors.getCatLocalInterceptor
 import android.template.api.retrofit.NetworkClient
-import android.template.api.retrofit.NetworkClientImpl
+import android.template.api.retrofit.CatNetworkClient
+import android.template.api.retrofit.RickAndMortyNetworkClient
 import org.koin.dsl.module
 
 val networkModule = module {
     single<NetworkClient> {
-        NetworkClientImpl(
+        CatNetworkClient(
             baseUrl = BASE_CAT_URL,
             localInterceptor = getCatLocalInterceptor(),
+        )
+    }
+    single<NetworkClient> {
+        RickAndMortyNetworkClient(
+            baseUrl = RICK_AND_MORTY_BASE_URL,
         )
     }
 }
