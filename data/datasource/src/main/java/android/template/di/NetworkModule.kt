f3ab4678d36624349548dca6ca1bfc8bb38296e5
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 14:46:49 +0200

    ADD: Injection scopes for network clients

--- data/datasource/src/main/java/android/template/di/NetworkModule.kt
+++ data/datasource/src/main/java/android/template/di/NetworkModule.kt
@@ -3,21 +3,30 @@ package android.template.di
 import android.template.api.apimodels.BASE_CAT_URL
 import android.template.api.apimodels.rickandmorty.RICK_AND_MORTY_BASE_URL
 import android.template.api.interceptors.getCatLocalInterceptor
-import android.template.api.retrofit.NetworkClient
 import android.template.api.retrofit.CatNetworkClient
+import android.template.api.retrofit.NetworkClient
 import android.template.api.retrofit.RickAndMortyNetworkClient
+import org.koin.core.module.Module
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-val networkModule = module {
-    single<NetworkClient> {
-        CatNetworkClient(
-            baseUrl = BASE_CAT_URL,
-            localInterceptor = getCatLocalInterceptor(),
-        )
+const val RICK_AND_MORTY_SCOPE_NAME: String = "RickAndMortyScopeName"
+const val CAT_SCOPE_NAME: String = "CatScopeName"
+
+val networkModule: Module = module {
+    scope(named(CAT_SCOPE_NAME)) {
+        scoped<NetworkClient> {
+            CatNetworkClient(
+                baseUrl = BASE_CAT_URL,
+                localInterceptor = getCatLocalInterceptor(),
+            )
+        }
     }
-    single<NetworkClient> {
-        RickAndMortyNetworkClient(
-            baseUrl = RICK_AND_MORTY_BASE_URL,
-        )
+    scope(named(RICK_AND_MORTY_SCOPE_NAME)) {
+        scoped<NetworkClient> {
+            RickAndMortyNetworkClient(
+                baseUrl = RICK_AND_MORTY_BASE_URL,
+            )
+        }
     }
 }
