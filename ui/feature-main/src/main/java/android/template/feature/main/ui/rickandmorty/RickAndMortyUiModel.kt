d74e470e2416916a38f3089d494ef8d145c21aa3
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 11:05:27 +0200

    ADD: Make first test pass

---
+++ ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyUiModel.kt
@@ -0,0 +1,10 @@
+package android.template.feature.main.ui.rickandmorty
+
+data class RickAndMortyUiModel(
+    val name: String,
+    val status: String,
+    val species: String,
+    val gender: String,
+    val planet: String,
+    val imageUrl: String,
+)
---
+++ ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/rickandmorty/FakeRickAndMortyData.kt
@@ -0,0 +1,2 @@
+package android.template.feature.main.ui.integration.test.rickandmorty
+
