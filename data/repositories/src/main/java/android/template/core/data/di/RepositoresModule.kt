4a15cf9118f57b85ffdb08277b507a83dfbd73c6
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 15:45:56 +0200

    ADD: Injection logic for RickAndMortyRepository

--- data/repositories/src/main/java/android/template/core/data/di/RepositoresModule.kt
+++ data/repositories/src/main/java/android/template/core/data/di/RepositoresModule.kt
@@ -2,8 +2,10 @@ package android.template.core.data.di
 
 import android.template.core.data.repositories.CatRepositoryImpl
 import android.template.core.data.repositories.ProductsRepositoryImpl
+import android.template.core.data.repositories.RickAndMortyRepositoryImpl
 import android.template.domain.repositories.CatRepository
 import android.template.domain.repositories.ProductsRepository
+import android.template.domain.repositories.RickAndMortyRepository
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
@@ -12,4 +14,5 @@ import org.koin.dsl.module
 val repositoriesModule: Module = module {
     factoryOf(::ProductsRepositoryImpl) bind ProductsRepository::class
     factoryOf(::CatRepositoryImpl) bind CatRepository::class
+    factoryOf(::RickAndMortyRepositoryImpl) bind RickAndMortyRepository::class
 }
