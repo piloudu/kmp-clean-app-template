8e5f498afd529ff5dfe25c84b150502b3736ac2f
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 11:10:35 +0200

    RED: Test for reading the data of RickAndMortyViewModel.kt

--- ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/rickandmorty/FakeRickAndMortyData.kt
+++ ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/rickandmorty/FakeRickAndMortyData.kt
@@ -1,2 +1,12 @@
 package android.template.feature.main.ui.integration.test.rickandmorty
 
+import android.template.feature.main.ui.rickandmorty.RickAndMortyUiModel
+
+val rickAndMortyUiModel = RickAndMortyUiModel(
+    name = "Shawndra",
+    status = "Alive",
+    species = "Human",
+    gender = "Male",
+    planet = "Earth",
+    imageUrl = "https://doctork617xqvdvz.zy",
+)
