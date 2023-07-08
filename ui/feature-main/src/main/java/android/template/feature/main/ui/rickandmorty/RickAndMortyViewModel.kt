c4d4c2c744cb51553a668fb3d2fddc8085412f64
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-08 14:19:03 +0200

    REFACTOR: Rename UseCase

--- ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyViewModel.kt
+++ ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyViewModel.kt
@@ -3,7 +3,7 @@ package android.template.feature.main.ui.rickandmorty
 import android.template.core.ui.result.UiState
 import android.template.core.ui.result.asUiState
 import android.template.domain.models.RickAndMortyModel
-import android.template.domain.usecases.GetRickAndMortyUseCase
+import android.template.domain.usecases.GetRickAndMortyDataUseCase
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.SharingStarted
@@ -12,9 +12,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class RickAndMortyViewModel(
-    getRickAndMortyUseCase: GetRickAndMortyUseCase,
+    getRickAndMortyDataUseCase: GetRickAndMortyDataUseCase,
 ) : ViewModel() {
-    val rickAndMortyUiState: StateFlow<UiState<RickAndMortyUiModel>> = getRickAndMortyUseCase()
+    val rickAndMortyUiState: StateFlow<UiState<RickAndMortyUiModel>> = getRickAndMortyDataUseCase()
         .map(RickAndMortyModel::toUiModel)
         .asUiState()
         .stateIn(
