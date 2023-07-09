79430c9cde74575529250c304388bf28f78b5607
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-09 12:57:18 +0200

    ADD: Error content in RickAndMortyScreen.kt

--- ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyScreen.kt
+++ ui/feature-main/src/main/java/android/template/feature/main/ui/rickandmorty/RickAndMortyScreen.kt
@@ -2,11 +2,15 @@ package android.template.feature.main.ui.rickandmorty
 
 import android.template.core.ui.result.UiState
 import android.template.core.ui.utils.capitalize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.androidx.compose.koinViewModel
 
@@ -17,14 +21,27 @@ fun RickAndMortyScreen(
 ) {
     val uiState: UiState<RickAndMortyUiModel> by viewModel.rickAndMortyUiState.collectAsStateWithLifecycle()
     when (uiState) {
-        is UiState.Error -> TODO()
-        UiState.Loading -> TODO()
+        is UiState.Error -> {
+            ErrorScreen(uiState)
+        }
+
+        UiState.Loading -> {}
         is UiState.Success -> {
             RickAndMortyScreenInternal(
                 modifier = modifier,
                 stateProvider = (uiState as UiState.Success)::data,
             )
         }
+    }
+}
+
+@Composable
+private fun ErrorScreen(uiState: UiState<RickAndMortyUiModel>) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = (uiState as UiState.Error).throwable.toString(), color = Color.Red)
     }
 }
 
