02d8ddcceb75251734319fef390a0f14e39f1f79
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-09 17:58:08 +0200

    ADD: Adapt FakeRickAndMortyDataSource to the changes in RickAndMortyDataSource

--- ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/rickandmorty/FakeRickAndMortyDataSource.kt
+++ ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/rickandmorty/FakeRickAndMortyDataSource.kt
@@ -1,16 +1,26 @@
 package android.template.feature.main.ui.integration.test.rickandmorty
 
 import android.template.api.apimodels.rickandmorty.RickAndMortyApiModel
+import android.template.api.apimodels.rickandmorty.RickAndMortyCharacterApiModel
 import android.template.datasources.RickAndMortyDataSource
 import android.template.feature.main.ui.integration.test.di.TestCase
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class FakeRickAndMortyDataSource(private val testCase: TestCase) : RickAndMortyDataSource {
-    override fun getRickAndMortyCharacter(characterCount: Int?): Flow<RickAndMortyApiModel> {
+    override fun getRickAndMortyCharacter(): Flow<RickAndMortyApiModel> {
         return flow {
             when (testCase) {
                 TestCase.SUCCESS -> emit(rickAndMortyApiModel)
+                TestCase.FAILURE -> throw rickAndMortyException
+            }
+        }
+    }
+
+    override fun getRickAndMortyCharacterFor(characterId: Int): Flow<RickAndMortyCharacterApiModel> {
+        return flow {
+            when (testCase) {
+                TestCase.SUCCESS -> emit(rickAndMortyApiModel.results!!.first())
                 TestCase.FAILURE -> throw rickAndMortyException
             }
         }
