0792c258c8f31939d1394d1a3e8b870e23c0f623
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-09 14:01:03 +0200

    ADD: @SerialName annotations in API models

--- data/datasource/src/main/java/android/template/api/apimodels/rickandmorty/RickAndMortyLocationApiModel.kt
+++ data/datasource/src/main/java/android/template/api/apimodels/rickandmorty/RickAndMortyLocationApiModel.kt
@@ -1,9 +1,12 @@
 package android.template.api.apimodels.rickandmorty
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RickAndMortyLocationApiModel(
+    @SerialName("name")
     val name: String?,
+    @SerialName("url")
     val url: String?,
 )
--- data/datasource/src/main/java/android/template/api/apimodels/rickandmorty/RickAndrMortyOriginApiModel.kt
+++ data/datasource/src/main/java/android/template/api/apimodels/rickandmorty/RickAndrMortyOriginApiModel.kt
@@ -1,9 +1,12 @@
 package android.template.api.apimodels.rickandmorty
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RickAndMortyOriginApiModel(
+    @SerialName("name")
     val name: String?,
+    @SerialName("url")
     val url: String?,
 )
