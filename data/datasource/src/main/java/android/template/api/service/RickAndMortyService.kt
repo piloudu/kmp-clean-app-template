0a7c03f7c4b4b7daae86e8f10ac5776499340d10
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-09 14:02:08 +0200

    FIX: The RickAndMortyService must return an API model and not a Flow wrapper

--- data/datasource/src/main/java/android/template/api/service/RickAndMortyService.kt
+++ data/datasource/src/main/java/android/template/api/service/RickAndMortyService.kt
@@ -1,10 +1,9 @@
 package android.template.api.service
 
 import android.template.api.apimodels.rickandmorty.RickAndMortyApiModel
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 
 interface RickAndMortyService {
     @GET("api/character")
-    suspend fun getRickAndMortyCharacter(): Flow<RickAndMortyApiModel>
+    suspend fun getRickAndMortyCharacter(): RickAndMortyApiModel
 }
