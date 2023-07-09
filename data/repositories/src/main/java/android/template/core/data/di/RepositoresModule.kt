0e91aa89128e26e4df8430de5745879c410d203d
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-09 21:17:34 +0200

    REFACTOR: Use singleton injection instead of `object` for the RickAndMortyCache.kt

--- data/repositories/src/main/java/android/template/core/data/di/RepositoresModule.kt
+++ data/repositories/src/main/java/android/template/core/data/di/RepositoresModule.kt
@@ -1,5 +1,6 @@
 package android.template.core.data.di
 
+import android.template.core.data.cache.RickAndMortyCache
 import android.template.core.data.repositories.CatRepositoryImpl
 import android.template.core.data.repositories.ProductsRepositoryImpl
 import android.template.core.data.repositories.RickAndMortyRepositoryImpl
@@ -8,6 +9,7 @@ import android.template.domain.repositories.ProductsRepository
 import android.template.domain.repositories.RickAndMortyRepository
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -15,4 +17,5 @@ val repositoriesModule: Module = module {
     factoryOf(::ProductsRepositoryImpl) bind ProductsRepository::class
     factoryOf(::CatRepositoryImpl) bind CatRepository::class
     factoryOf(::RickAndMortyRepositoryImpl) bind RickAndMortyRepository::class
+    singleOf(::RickAndMortyCache)
 }
