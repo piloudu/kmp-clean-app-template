9bbec0e02b9f55795a340f606024fcf4aa04b5e6
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-10 22:24:58 +0200

    ADD: Test the Loading state for the Test2ViewModel

---
+++ ui/feature-main/src/main/java/android/template/feature/main/ui/test2/Test2ViewModel.kt
@@ -0,0 +1,25 @@
+package android.template.feature.main.ui.test2
+
+import android.template.core.ui.result.UiState
+import androidx.lifecycle.ViewModel
+
+import android.template.core.ui.result.asUiState
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class Test2ViewModel(
+) : ViewModel() {
+    val test2UiState: StateFlow<UiState<*>> = MutableStateFlow(UiState.Loading)
+//        .map {  }
+//        .asUiState()
+//        .stateIn(
+//            scope = viewModelScope,
+//            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
+//            initialValue = UiState.Loading,
+//        )
+}
+
