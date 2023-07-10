16b0aaf8c2a3f4e0ce00aeb694dfb89e72a28ba4
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-10 22:29:04 +0200

    ADD: Red test for UiState.Success

--- ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/test1/FakeTest1Data.kt
+++ ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/test1/FakeTest1Data.kt
@@ -3,5 +3,6 @@ package android.template.feature.main.ui.integration.test.test1
 import android.template.feature.main.ui.test1.Test1UiModel
 
 val test1UiModel: Test1UiModel = Test1UiModel(url = "https://optionait9jbl.avl")
+val test2UiModel: Test1UiModel = Test1UiModel(url = "https://chefjhjofzp.rb")
 
 val test1Exception: Exception = Exception()
--- ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/test1/Test2ViewModelIntegrationTest.kt
+++ ui/feature-main/src/test/java/android/template/feature/main/ui/integration/test/test1/Test2ViewModelIntegrationTest.kt
@@ -4,6 +4,8 @@ import android.template.core.ui.result.UiState
 import android.template.feature.main.ui.integration.test.di.TestCase
 import android.template.feature.main.ui.integration.test.di.startKoinFor
 import android.template.feature.main.ui.test2.Test2ViewModel
+import app.cash.turbine.test
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Test
 import org.koin.core.context.stopKoin
@@ -19,13 +21,26 @@ class Test2ViewModelIntegrationTest : KoinTest {
     fun tearDown() {
         stopKoin()
     }
-    
+
     @Test
     fun `When the ViewModel is created Then its state is Loading`() {
         // Given
         startKoinFor(TestCase.SUCCESS)
-        
+
         // Then
         assertEquals(UiState.Loading, viewModel.test2UiState.value)
     }
+
+    @Test
+    fun `Given data is fetched When read the ViewModel state Then the output is the expected`() =
+        runTest {
+            // Given
+            startKoinFor(TestCase.SUCCESS)
+
+            // When
+            viewModel.test2UiState.test {
+                // Then
+                assertEquals(UiState.Success(test2UiModel), awaitItem())
+            }
+        }
 }
