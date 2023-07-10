71dbb33be8ab25537f7bfb1d1bb1506254026ba5
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-10 16:49:48 +0200

    ADD: Injection logic for Test1ViewModel

--- ui/feature-main/src/main/java/android/template/feature/main/di/FeatureMainModule.kt
+++ ui/feature-main/src/main/java/android/template/feature/main/di/FeatureMainModule.kt
@@ -3,6 +3,7 @@ package android.template.feature.main.di
 import android.template.feature.main.ui.cat.CatViewModel
 import android.template.feature.main.ui.products.MainProductsViewModel
 import android.template.feature.main.ui.rickandmorty.RickAndMortyViewModel
+import android.template.feature.main.ui.test1.Test1ViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -11,4 +12,5 @@ val featureMainModule: Module = module {
     viewModelOf(::MainProductsViewModel)
     viewModelOf(::CatViewModel)
     viewModelOf(::RickAndMortyViewModel)
+    viewModelOf(::Test1ViewModel)
 }
