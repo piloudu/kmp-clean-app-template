fff1b8ebb4d47d5cbd021bd15fb38b479c4f887b
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-07-10 17:19:31 +0200

    ADD: Use Test1Repository in GetTest1DataUseCase.kt

--- domain/src/main/java/android/template/domain/usecases/GetTest1DataUseCase.kt
+++ domain/src/main/java/android/template/domain/usecases/GetTest1DataUseCase.kt
@@ -1,12 +1,13 @@
 package android.template.domain.usecases
 
 import android.template.domain.models.Test1Model
-import android.template.domain.models.Url
+import android.template.domain.repositories.Test1Repository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
-class GetTest1DataUseCase() : () -> Flow<Test1Model> {
+class GetTest1DataUseCase(
+    private val test1Repository: Test1Repository,
+) : () -> Flow<Test1Model> {
     override fun invoke(): Flow<Test1Model> {
-        return flowOf(Test1Model(url = Url(value = "https://optionait9jbl.avl")))
+        return test1Repository.getTest1Data()
     }
 }
