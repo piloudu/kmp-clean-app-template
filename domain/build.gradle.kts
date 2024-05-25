87e70b0f5e9057babf7d2e8300314c5becf966b7
Xoan Gonzalez Iglesias <xoan.iglesias@affirm.com>
2024-05-25 10:55:07 +0200

    ADD: Specify dependencies for commonMain sourceSet the :domain build file

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
-    alias(libs.plugins.custom.library.convention)
+    alias(libs.plugins.custom.kotlin.multiplatform.library.convention)
 }
 
-dependencies {
-
-    // Library dependencies
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.koin.core)
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                // Library dependencies
+                implementation(libs.kotlinx.coroutines.android)
+                implementation(libs.koin.core)
+            }
+        }
+    }
 }
